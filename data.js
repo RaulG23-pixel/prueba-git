7744c02da4de85e94551e227210756d9ea03c356
Raul Gonzalez <raulx12@gmail.com>
2020-12-31 09:25:14 -0430

    actualizacion de numeros en data.js

--- data.js
+++ data.js
@@ -1,3 +1,3 @@
 const arreglo = [{"name": "Raul Gonzalez", age: 24}, {"name": "Alejandro Gutierrez", age: 22} , {"name": "Fabian Castro", age: 23}];
-
+const numeros = [1,2,3,4,5];
 module.exports = arreglo;
